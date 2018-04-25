8c97c6e77bfbe924a03f6cb0f02b596bbe5abfd4
yoneyama ryo <r.yoneyama@team.nri-net.com>
2018-04-25 10:13:38 +0900

    repair serviceworker

--- sw.js
+++ sw.js
@@ -1,61 +1,55 @@
 // ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
 
 // キャッシュ名とキャッシュファイルの指定
-var CACHE_NAME = 'pwa-cache-sample';
+var CACHE_NAME = 'pwa-cache-sample1';
 var urlsToCache = [
-	'./index.html',
-	'./link.html',
-	'./link2.html',
-	'./scripts/main.js',
-	'./css/normalize.css',
-	'./images/computer_smartphone_connect.png'
+	'/',
+	'/index.html',
+	'/link.html',
+	'/link2.html',
+	'/scripts/main.js',
+	'/css/normalize.css',
+	'/images/computer_smartphone_connect.png'
 ];
 
 // インストール処理
 self.addEventListener('install', function(event) {
+	console.log('[ServiceWorker] Install')
 	event.waitUntil(
 		caches
 			.open(CACHE_NAME)
 			.then(function(cache) {
+				console.log('[ServiceWorker] Cache App Data')
 				return cache.addAll(urlsToCache);
 			})
 
 	);
 });
 
+// ページ更新時の新キャッシュ取得と旧キャッシュの削除
+self.addEventListener('activate', function(event) {
+	console.log('[ServiceWorker] Activate')
+	event.waitUntil(
+		caches
+			.keys().then(function(keyList) {
+				return Promise.all(keyList.map(function(key) {
+					console.log('[SerivceWorker] Removing old cache', key);
+					if ( key !== CACHE_NAME){
+						return caches.delete(key);
+					}
+				}))
+			})
+	)
+})
+
 // リソースフェッチ時のキャッシュロード処理
 self.addEventListener('fetch', function(event) {
+	console.log('[ServiceWorker] Fetch', event.request.url);
 	event.respondWith(
 		caches
 			.match(event.request)
 			.then(function(response) {
-				if (response) {
-					return response;
-				}
-
-				// 重要：リクエストを clone する。リクエストは Stream なので
-				// 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
-				// 必要なので、リクエストは clone しないといけない
-				let fetchRequest = event.request.clone();
-
-				return fetch(fetchRequest)
-					.then((response) => {
-						if (!response || response.status !== 200 || response.type !== 'basic') {
-							return response;
-						}
-
-						// 重要：レスポンスを clone する。レスポンスは Stream で
-						// ブラウザ用とキャッシュ用の2回必要。なので clone して
-						// 2つの Stream があるようにする
-						let responseToCache = response.clone();
-
-						caches.open(CACHE_NAME)
-							  .then((cache) => {
-								  cache.put(event.request, responseToCache);
-							  });
-
-						return response;
-					});
+				return response || fetch(event.request);
 			})
 	);
 });
