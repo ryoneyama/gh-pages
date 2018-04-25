f0f89f36e362ecc891526612ce2eb920f889c1bc
yoneyama ryo <r.yoneyama@team.nri-net.com>
2018-04-25 10:44:07 +0900

    repair site

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
 
 // キャッシュ名とキャッシュファイルの指定
-var CACHE_NAME = 'pwa-cache-sample1';
+var CACHE_NAME = 'pwa-cache-sample2';
 var urlsToCache = [
 	'./index.html',
 	'./link.html',
